4540cc12828f505bd145eb04291b6475b65b111a
vjuge <vincent.juge@gmail.com>
2022-07-04 23:03:34 +0200

    wip - tuning

---
+++ modules/P6/mlflow_tracking.py
@@ -0,0 +1,23 @@
+import os
+from random import random, randint
+import mlflow
+from mlflow import log_metric, log_param, log_artifacts
+
+if __name__ == "__main__":
+    mlflow.set_tracking_uri("databricks")
+    mlflow.set_experiment("/my-experiment")
+    
+    # Log a parameter (key-value pair)
+    log_param("param1", randint(0, 100))
+
+    # Log a metric; metrics can be updated throughout the run
+    log_metric("foo", random())
+    log_metric("foo", random() + 1)
+    log_metric("foo", random() + 2)
+
+    # Log an artifact (output file)
+    if not os.path.exists("outputs"):
+        os.makedirs("outputs")
+    with open("outputs/test.txt", "w") as f:
+        f.write("hello world!")
+    log_artifacts("outputs")
